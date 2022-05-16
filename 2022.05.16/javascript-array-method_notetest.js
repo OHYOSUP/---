e9190277f0f485ec5464305ae8ebee1cbede8a14
yosub <dhdytjq2@naver.com>
2022-05-16 18:01:25 +0900

    배열함수

---
+++ 2022.05.16/javascript-array-method_notetest.js
@@ -0,0 +1,13 @@
+const arrExample = ["피카츄", "라이츄", "파이리", "꼬부기", "버터풀", "야도란" , "피존투", "또가스", "우린", "서로", "모습은", "달라도"]
+
+// arrExample.forEach(Element=> console.log(arrExample[1]));
+
+// console.log(arrExample.push('뮤츠'));
+// console.log(arrExample);
+
+// console.log(arrExample.slice(2,4));
+// console.log(arrExample);
+
+// console.log(Array.isArray(arrExample));
+
+console.log(arrExample.at(3));
