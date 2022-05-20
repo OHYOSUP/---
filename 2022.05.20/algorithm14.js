c96bcc16f530c31ddb2ba4b37db875a01f86e516
yosub <dhdytjq2@naver.com>
2022-05-20 10:51:50 +0900

    algorithm14 답

--- 2022.05.20/algorithm14.js
+++ 2022.05.20/algorithm14.js
@@ -8,9 +8,14 @@ const arr = [ "a", "b", "c"];
 // console.log(newElem);
 // const b = arr.push(newElem);
 // console.log(arr);
-
-
-
+// =
+// let temp;
+// temp = arr[0];
+// console.log(temp);
+// arr.shift();
+// console.log(arr);
+// arr.push(temp);
+// console.log(arr);
 
 // 2. 원소값중 "c"를 "a" 앞으로 놓는 방법이 있을까?
 
@@ -22,7 +27,10 @@ const arr = [ "a", "b", "c"];
 // 3. 혹은 ["b", "a", "c"] 형태로 바꿀 수 있을까?
 
 
-// const newElem = arr.shift();
-// console.log(newElem);
-// const b = arr.push(newElem);
+let temp = arr.shift();
+// console.log(temp);
 // console.log(arr);
+
+arr.splice(1,0,temp);
+console.log(arr);
+
