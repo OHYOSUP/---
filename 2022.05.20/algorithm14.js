b1f839689e91c7f76796e1ccc27d5f998b543dfb
yosub <dhdytjq2@naver.com>
2022-05-20 09:41:14 +0900

    algorithm14

---
+++ 2022.05.20/algorithm14.js
@@ -0,0 +1,28 @@
+
+const arr = [ "a", "b", "c"];
+
+
+// 1. 원소값 중 "a"를 "c" 다음으로 놓는 방법이 있을까?
+
+// const newElem = arr.shift();
+// console.log(newElem);
+// const b = arr.push(newElem);
+// console.log(arr);
+
+
+
+
+// 2. 원소값중 "c"를 "a" 앞으로 놓는 방법이 있을까?
+
+
+// const pop = arr.pop();
+// arr.unshift(pop);
+// console.log(arr);
+
+// 3. 혹은 ["b", "a", "c"] 형태로 바꿀 수 있을까?
+
+
+// const newElem = arr.shift();
+// console.log(newElem);
+// const b = arr.push(newElem);
+// console.log(arr);
