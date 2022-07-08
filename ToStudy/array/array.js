5f293bacff5dc0004837cfbd4492d361bb65c8d5
yosub <dhdytjq2@naver.com>
2022-07-08 13:00:23 +0900

    pageTransition

---
+++ ToStudy/array/array.js
@@ -0,0 +1,16 @@
+let temp;
+let ahjin = [1, 2];
+
+temp = ahjin[0];
+ahjin[0] = ahjin[1];
+ahjin[1] = temp;
+
+console.log(ahjin);
+
+// let newAhjin = ahjin.shift();
+// console.log(newAhjin);
+
+// ahjin.push(newAhjin);
+
+// console.log(ahjin);
+
